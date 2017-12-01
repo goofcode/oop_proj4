bda3126790a3997ca30aca3e881cef13ec7af6f9
Mookeun Ji <goofcode@gmail.com>
2017-12-02 04:04:33 +0900

    Add Transaction class and table

---
+++ oop_proj4/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace oop_proj4
+{
+    [Table(Name = "Transaction")]
+    public class Transaction
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)] public int Id { get; set; }
+        [Column] public string MemberId { get; set; }
+        [Column] public DateTime Date { get; set; }
+        [Column] public int Type { get; set; }
+        [Column] public int PayMethod { get; set; }
+        [Column] public string Memo { get; set; }
+    }
+}
