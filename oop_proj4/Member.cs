8c1b0a358337cb24bec4cf8011c29ec8fc078f42
Mookeun Ji <goofcode@gmail.com>
2017-12-02 07:32:01 +0900

    Remove default db creation

--- oop_proj4/Member.cs
+++ oop_proj4/Member.cs
@@ -6,6 +6,9 @@ namespace oop_proj4
     [Table(Name = "Member")]
     public class Member
     {
+        public enum GENDER { MALE = 1, FEMALE = 2 };
+        public enum REGISTERATION_STATE{ ACTIVE = 1, DORMANT = 3 };
+
         [Column(IsPrimaryKey = true, IsDbGenerated = true)] public int Id { get; set; }
         [Column] public string Name { get; set; }
         [Column] public string Tel { get; set; }
@@ -13,6 +16,7 @@ namespace oop_proj4
         [Column] public DateTime BirthDate { get; set; }
         [Column] public int RegisterationState { get; set; }
         [Column] public int LeftDay { get; set; }
+        [Column] public DateTime RegisterationDate { get; set; }
         [Column] public string Memo { get; set; }
     }
 }
