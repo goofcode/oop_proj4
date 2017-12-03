d03bc3dbb4f3e952f8d492c0c5fd949b42607f58
kor-Chipmunk <rhj4862@cau.ac.kr>
2017-12-04 01:54:06 +0900

    Add LoginForm Background & Fix WinForms' titles

--- oop_proj4/Form/EditAdminForm.Designer.cs
+++ oop_proj4/Form/EditAdminForm.Designer.cs
@@ -35,12 +35,15 @@
             this.ChangePasswordTxt = new Telerik.WinControls.UI.RadTextBox();
             this.ChangePasswordBtn = new Telerik.WinControls.UI.RadButton();
             this.ConfirmPasswordTxt = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.lblSeperator = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.CurrentPasswordTxt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ChangePasswordTxt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ChangePasswordBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ConfirmPasswordTxt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,67 +51,90 @@
             // 
             this.CurrentPasswordTxt.AutoSize = false;
             this.CurrentPasswordTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CurrentPasswordTxt.Location = new System.Drawing.Point(31, 87);
+            this.CurrentPasswordTxt.Location = new System.Drawing.Point(185, 26);
             this.CurrentPasswordTxt.Name = "CurrentPasswordTxt";
             this.CurrentPasswordTxt.PasswordChar = '●';
             this.CurrentPasswordTxt.Size = new System.Drawing.Size(200, 30);
             this.CurrentPasswordTxt.TabIndex = 1;
+            this.CurrentPasswordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.CurrentPasswordTxt.UseSystemPasswordChar = true;
             // 
             // radLabel1
             // 
             this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel1.Location = new System.Drawing.Point(31, 44);
+            this.radLabel1.Location = new System.Drawing.Point(12, 26);
             this.radLabel1.Name = "radLabel1";
-            this.radLabel1.Size = new System.Drawing.Size(128, 29);
+            this.radLabel1.Size = new System.Drawing.Size(138, 29);
             this.radLabel1.TabIndex = 0;
-            this.radLabel1.Text = "현재 비밀번호";
+            this.radLabel1.Text = "현재 비밀번호 :";
             // 
             // radLabel3
             // 
             this.radLabel3.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel3.Location = new System.Drawing.Point(31, 141);
+            this.radLabel3.Location = new System.Drawing.Point(12, 63);
             this.radLabel3.Name = "radLabel3";
-            this.radLabel3.Size = new System.Drawing.Size(147, 29);
+            this.radLabel3.Size = new System.Drawing.Size(157, 29);
             this.radLabel3.TabIndex = 1;
-            this.radLabel3.Text = "변경할 비밀번호";
+            this.radLabel3.Text = "변경할 비밀번호 :";
             // 
             // ChangePasswordTxt
             // 
             this.ChangePasswordTxt.AutoSize = false;
             this.ChangePasswordTxt.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ChangePasswordTxt.Location = new System.Drawing.Point(31, 185);
+            this.ChangePasswordTxt.Location = new System.Drawing.Point(185, 62);
             this.ChangePasswordTxt.Name = "ChangePasswordTxt";
             this.ChangePasswordTxt.PasswordChar = '●';
             this.ChangePasswordTxt.Size = new System.Drawing.Size(200, 30);
             this.ChangePasswordTxt.TabIndex = 3;
+            this.ChangePasswordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.ChangePasswordTxt.UseSystemPasswordChar = true;
             // 
             // ChangePasswordBtn
             // 
-            this.ChangePasswordBtn.Location = new System.Drawing.Point(286, 177);
+            this.ChangePasswordBtn.Location = new System.Drawing.Point(12, 175);
             this.ChangePasswordBtn.Name = "ChangePasswordBtn";
-            this.ChangePasswordBtn.Size = new System.Drawing.Size(99, 74);
+            this.ChangePasswordBtn.Size = new System.Drawing.Size(373, 37);
             this.ChangePasswordBtn.TabIndex = 4;
-            this.ChangePasswordBtn.Text = "변경";
+            this.ChangePasswordBtn.Text = "변경하기";
             this.ChangePasswordBtn.ThemeName = "Windows8";
             // 
             // ConfirmPasswordTxt
             // 
             this.ConfirmPasswordTxt.AutoSize = false;
             this.ConfirmPasswordTxt.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ConfirmPasswordTxt.Location = new System.Drawing.Point(31, 221);
+            this.ConfirmPasswordTxt.Location = new System.Drawing.Point(185, 99);
             this.ConfirmPasswordTxt.Name = "ConfirmPasswordTxt";
             this.ConfirmPasswordTxt.PasswordChar = '●';
             this.ConfirmPasswordTxt.Size = new System.Drawing.Size(200, 30);
             this.ConfirmPasswordTxt.TabIndex = 4;
+            this.ConfirmPasswordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.ConfirmPasswordTxt.UseSystemPasswordChar = true;
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.Location = new System.Drawing.Point(12, 99);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(138, 29);
+            this.radLabel2.TabIndex = 2;
+            this.radLabel2.Text = "비밀번호 확인 :";
+            // 
+            // lblSeperator
+            // 
+            this.lblSeperator.BackColor = System.Drawing.Color.Red;
+            this.lblSeperator.Location = new System.Drawing.Point(10, 158);
+            this.lblSeperator.Name = "lblSeperator";
+            this.lblSeperator.Size = new System.Drawing.Size(375, 1);
+            this.lblSeperator.TabIndex = 13;
             // 
             // EditAdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 283);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(401, 225);
+            this.Controls.Add(this.lblSeperator);
+            this.Controls.Add(this.radLabel2);
             this.Controls.Add(this.ConfirmPasswordTxt);
             this.Controls.Add(this.ChangePasswordBtn);
             this.Controls.Add(this.ChangePasswordTxt);
@@ -121,7 +147,8 @@
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.Text = "EditAdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "주짓수 회원관리 프로그램 - [관리자 수정하기]";
             this.ThemeName = "Windows8";
             ((System.ComponentModel.ISupportInitialize)(this.CurrentPasswordTxt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
@@ -129,6 +156,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.ChangePasswordTxt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ChangePasswordBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ConfirmPasswordTxt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -142,5 +170,7 @@
         private Telerik.WinControls.UI.RadTextBox ChangePasswordTxt;
         private Telerik.WinControls.UI.RadButton ChangePasswordBtn;
         private Telerik.WinControls.UI.RadTextBox ConfirmPasswordTxt;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private System.Windows.Forms.Label lblSeperator;
     }
 }
--- oop_proj4/Form/NewAdminForm.Designer.cs
+++ oop_proj4/Form/NewAdminForm.Designer.cs
@@ -32,15 +32,18 @@
             this.ConfirmNewPasswordTxt = new Telerik.WinControls.UI.RadTextBox();
             this.SubmintBtn = new Telerik.WinControls.UI.RadButton();
             this.NewPasswordTxt = new Telerik.WinControls.UI.RadTextBox();
-            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
-            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.lblPW = new Telerik.WinControls.UI.RadLabel();
+            this.lblID = new Telerik.WinControls.UI.RadLabel();
             this.NewIDTxt = new Telerik.WinControls.UI.RadTextBox();
+            this.lblPWConfirm = new Telerik.WinControls.UI.RadLabel();
+            this.lblSeperator = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ConfirmNewPasswordTxt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SubmintBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NewPasswordTxt)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblID)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NewIDTxt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPWConfirm)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,84 +51,110 @@
             // 
             this.ConfirmNewPasswordTxt.AutoSize = false;
             this.ConfirmNewPasswordTxt.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ConfirmNewPasswordTxt.Location = new System.Drawing.Point(21, 199);
+            this.ConfirmNewPasswordTxt.Location = new System.Drawing.Point(126, 95);
             this.ConfirmNewPasswordTxt.Name = "ConfirmNewPasswordTxt";
             this.ConfirmNewPasswordTxt.PasswordChar = '●';
             this.ConfirmNewPasswordTxt.Size = new System.Drawing.Size(200, 30);
             this.ConfirmNewPasswordTxt.TabIndex = 9;
+            this.ConfirmNewPasswordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.ConfirmNewPasswordTxt.UseSystemPasswordChar = true;
             // 
             // SubmintBtn
             // 
-            this.SubmintBtn.Location = new System.Drawing.Point(276, 155);
+            this.SubmintBtn.Location = new System.Drawing.Point(12, 162);
             this.SubmintBtn.Name = "SubmintBtn";
-            this.SubmintBtn.Size = new System.Drawing.Size(99, 74);
+            this.SubmintBtn.Size = new System.Drawing.Size(314, 34);
             this.SubmintBtn.TabIndex = 10;
-            this.SubmintBtn.Text = "추가";
+            this.SubmintBtn.Text = "추가하기";
             this.SubmintBtn.ThemeName = "Windows8";
             // 
             // NewPasswordTxt
             // 
             this.NewPasswordTxt.AutoSize = false;
             this.NewPasswordTxt.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NewPasswordTxt.Location = new System.Drawing.Point(21, 163);
+            this.NewPasswordTxt.Location = new System.Drawing.Point(126, 59);
             this.NewPasswordTxt.Name = "NewPasswordTxt";
             this.NewPasswordTxt.PasswordChar = '●';
             this.NewPasswordTxt.Size = new System.Drawing.Size(200, 30);
             this.NewPasswordTxt.TabIndex = 8;
+            this.NewPasswordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.NewPasswordTxt.UseSystemPasswordChar = true;
             // 
-            // radLabel3
+            // lblPW
             // 
-            this.radLabel3.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel3.Location = new System.Drawing.Point(21, 119);
-            this.radLabel3.Name = "radLabel3";
-            this.radLabel3.Size = new System.Drawing.Size(85, 29);
-            this.radLabel3.TabIndex = 6;
-            this.radLabel3.Text = "비밀번호";
+            this.lblPW.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPW.Location = new System.Drawing.Point(21, 60);
+            this.lblPW.Name = "lblPW";
+            this.lblPW.Size = new System.Drawing.Size(94, 29);
+            this.lblPW.TabIndex = 6;
+            this.lblPW.Text = "비밀번호 :";
             // 
-            // radLabel1
+            // lblID
             // 
-            this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel1.Location = new System.Drawing.Point(21, 22);
-            this.radLabel1.Name = "radLabel1";
-            this.radLabel1.Size = new System.Drawing.Size(66, 29);
-            this.radLabel1.TabIndex = 5;
-            this.radLabel1.Text = "아이디";
+            this.lblID.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(21, 22);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(75, 29);
+            this.lblID.TabIndex = 5;
+            this.lblID.Text = "아이디 :";
             // 
             // NewIDTxt
             // 
             this.NewIDTxt.AutoSize = false;
-            this.NewIDTxt.Location = new System.Drawing.Point(21, 57);
+            this.NewIDTxt.Location = new System.Drawing.Point(126, 23);
             this.NewIDTxt.Name = "NewIDTxt";
             this.NewIDTxt.Size = new System.Drawing.Size(200, 30);
             this.NewIDTxt.TabIndex = 11;
+            this.NewIDTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblPWConfirm
+            // 
+            this.lblPWConfirm.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPWConfirm.Location = new System.Drawing.Point(21, 95);
+            this.lblPWConfirm.Name = "lblPWConfirm";
+            this.lblPWConfirm.Size = new System.Drawing.Size(56, 29);
+            this.lblPWConfirm.TabIndex = 7;
+            this.lblPWConfirm.Text = "확인 :";
+            // 
+            // lblSeperator
+            // 
+            this.lblSeperator.BackColor = System.Drawing.Color.Red;
+            this.lblSeperator.Location = new System.Drawing.Point(7, 147);
+            this.lblSeperator.Name = "lblSeperator";
+            this.lblSeperator.Size = new System.Drawing.Size(319, 1);
+            this.lblSeperator.TabIndex = 12;
             // 
             // NewAdminForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 251);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 210);
+            this.Controls.Add(this.lblSeperator);
+            this.Controls.Add(this.lblPWConfirm);
             this.Controls.Add(this.NewIDTxt);
             this.Controls.Add(this.ConfirmNewPasswordTxt);
             this.Controls.Add(this.SubmintBtn);
             this.Controls.Add(this.NewPasswordTxt);
-            this.Controls.Add(this.radLabel3);
-            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.lblPW);
+            this.Controls.Add(this.lblID);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "NewAdminForm";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.Text = "NewAdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "주짓수 회원관리 프로그램 - [관리자 계정 추가]";
             this.ThemeName = "Windows8";
             ((System.ComponentModel.ISupportInitialize)(this.ConfirmNewPasswordTxt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.SubmintBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NewPasswordTxt)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblID)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NewIDTxt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPWConfirm)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -137,8 +166,10 @@
         private Telerik.WinControls.UI.RadTextBox ConfirmNewPasswordTxt;
         private Telerik.WinControls.UI.RadButton SubmintBtn;
         private Telerik.WinControls.UI.RadTextBox NewPasswordTxt;
-        private Telerik.WinControls.UI.RadLabel radLabel3;
-        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel lblPW;
+        private Telerik.WinControls.UI.RadLabel lblID;
         private Telerik.WinControls.UI.RadTextBox NewIDTxt;
+        private Telerik.WinControls.UI.RadLabel lblPWConfirm;
+        private System.Windows.Forms.Label lblSeperator;
     }
 }
